6a3d75a9267ec4abcf1d69e6540cf45563fe0c33
Liya Tegared <liyategared7@gmail.com>
2025-12-07 23:27:48 +0300

    Time: 0 ms (100.00%) | Memory: 12.4 MB (84.24%) - LeetSync

---
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        i = 0   # pointer for nums1
+        j = 0   # pointer for nums2
+        newar = []
+
+        # merge until one list is finished
+        while i < m and j < n:
+            if nums1[i] <= nums2[j]:
+                newar.append(nums1[i])
+                i += 1
+            else:
+                newar.append(nums2[j])
+                j += 1
+
+        # add leftovers from nums1 (only from the real part up to m)
+        while i < m:
+            newar.append(nums1[i])
+            i += 1
+
+        # add leftovers from nums2
+        while j < n:
+            newar.append(nums2[j])
+            j += 1
+
+        # modify nums1 *in-place*
+        for k in range(m + n):
+            nums1[k] = newar[k]
