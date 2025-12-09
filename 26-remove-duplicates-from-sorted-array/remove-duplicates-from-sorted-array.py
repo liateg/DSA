70975614ae0e6cf48f524c7525cd1d14f97629f9
Liya Tegared <liyategared7@gmail.com>
2025-12-09 23:06:08 +0300

    Time: 111 ms (5.02%) | Memory: 13.1 MB (97.59%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -4,13 +4,14 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-       
-        for i in range(len(nums)):
-            if nums[i]!= '_' and nums.count(nums[i])>1 :
-                for j in range(1,nums.count(nums[i])):
-                    nums.remove(nums[i])
-                    nums.append('_')
-                
-            
-
-        return len(nums)-nums.count('_')
+        nr=[nums[0]]
+        c=0
+        for i in nums:
+            c=0
+            for j in range(len(nr)):
+                if i == nr[j]:
+                    c+=1
+            if c==0:
+                nr.append(i)
+        nums[:]=nr + ['_'] * (len(nums)-len(nr))
+        return len(nr)
