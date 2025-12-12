2b12ae0ebcfe74b863f489b404e80c42b0171a6a
Liya Tegared <liyategared7@gmail.com>
2025-12-12 23:11:54 +0300

    Time: 0 ms (100.00%) | Memory: 13.8 MB (29.22%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -4,14 +4,26 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        frq={}
-        res,mc=0,0
+        # frq={}
+        # res,mc=0,0
+        # for i in nums:
+        #     frq[i]=1+frq.get(i,0)
+        #     if frq[i]>mc:
+        #         mc=frq[i]
+        #         res=i
+        # return res
+
+        res=nums[0]
+        c=0
         for i in nums:
-            frq[i]=1+frq.get(i,0)
-            if frq[i]>mc:
-                mc=frq[i]
+            if c==0:
                 res=i
+            if res==i:
+                c+=1
+            else:
+                c-=1
         return res
+           
 
 
 
