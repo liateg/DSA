37d5572258d38785f5999de7c5804cbc54fa265a
Liya Tegared <liyategared7@gmail.com>
2026-07-17 21:05:41 +0300

    Implement list comprehension for input coordinates

---
+++ List Comprehensions/List Comprehensions.py
@@ -0,0 +1,15 @@
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+    r=[]
+    for i in range(x+1):
+        for j in range(y+1):
+            for k in range(z+1):
+                if k+i+j!=n:
+                    r.append([i,j,k])
+    print(r)
+    
+    
+
